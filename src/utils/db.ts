72ff7473dfa5852556def5b716d83458e4ae9fb9
showrav <showravdas8@gmail.com>
2025-02-18 21:56:27 +0600

    db connection update

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.w5uf7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w5uf7.mongodb.net/${process.env.DB_USER}?retryWrites=true&w=majority&appName=Cluster0`
     );
     console.log("MongoDB connected");
   } catch (error) {
